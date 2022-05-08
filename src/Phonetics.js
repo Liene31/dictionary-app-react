25d5c0b0c2e76f676839438198e7d834d89c962c
Liene31 <lienebare@gmail.com>
2022-05-08 20:55:00 +0200

    Added audio button

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetics(props) {
-  console.log(props.phonetics.audio);
-  return (
-    <div className="Phonetics">
-      <p>{props.phonetics.text}</p>
-      <a href={props.phonetics.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
-    </div>
-  );
+  const [play, setPlay] = useState(false);
+
+  function handleClick() {
+    setPlay(true);
+  }
+
+  if (props.phonetics.text && props.phonetics.audio) {
+    return (
+      <div className="Phonetics">
+        <p>{props.phonetics.text}</p>
+        <button onClick={handleClick}>
+          <i className="fa fa-volume-up"></i>
+        </button>
+        {play && (
+          <ReactAudioPlayer
+            src={props.phonetics.audio}
+            autoPlay
+            onEnded={() => setPlay(false)}
+          />
+        )}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
