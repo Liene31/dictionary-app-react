7542277c7d05c9506628f2e71a977f2b72c7b42b
Liene31 <lienebare@gmail.com>
2022-05-10 19:35:11 +0200

    Styling

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,13 +1,14 @@
 import React from "react";
+import "./Synonyms.css"
 
 export default function Synonyms(props) {  
 
   if (props.synonyms.length !== 0) {
     return (
       <div className="Synonyms">
-        <span>Synonyms: </span>
+        <span className="synonym">Synonyms: </span>
         {props.synonyms.map(function (synonyms, index) {
-          return <span key={index}>{synonyms} </span>;
+          return <span className="synonym-example" key={index}>{synonyms} </span>;
         })}
       </div>
     );
